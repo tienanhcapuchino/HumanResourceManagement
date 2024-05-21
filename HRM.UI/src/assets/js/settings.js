074f9a28028c17b9a26c7f5c65e0bd8603b1aa86
ThaiDQ <88481930+thaidinhquang05@users.noreply.github.com>
2024-05-21 21:56:32 +0700

    update UI
    * add mysql connection string
    
    * get total earning
    
    * update something cool
    
    * update UI for HRM
    
    * update UI

--- HRM.UI/src/assets/js/settings.js
+++ HRM.UI/src/assets/js/settings.js
@@ -1,17 +1,21 @@
 (function($) {
   'use strict';
   $(function() {
-    $(".nav-settings").on("click", function() {
+    $(".nav-settings").click(function() {
       $("#right-sidebar").toggleClass("open");
     });
-    $(".settings-close").on("click", function() {
-      $("#right-sidebar,#theme-settings").removeClass("open");
+    $(".settings-close").click(function() {
+      $("#right-sidebar,#theme-settings,#layout-settings").removeClass("open");
     });
 
-    $("#settings-trigger").on("click" , function(){
+    $("#settings-trigger").on("click", function() {
       $("#theme-settings").toggleClass("open");
     });
 
+    //check all boxes in order status 
+    $("#check-all").click(function () {
+      $(".checkbox").prop('checked', $(this).prop('checked'));
+    });
 
     //background constants
     var navbar_classes = "navbar-danger navbar-success navbar-warning navbar-dark navbar-light navbar-primary navbar-info navbar-pink";
@@ -19,91 +23,100 @@
     var $body = $("body");
 
     //sidebar backgrounds
-    $("#sidebar-light-theme").on("click" , function(){
+    $("#sidebar-default-theme").on("click", function() {
       $body.removeClass(sidebar_classes);
-      $body.addClass("sidebar-light");
       $(".sidebar-bg-options").removeClass("selected");
       $(this).addClass("selected");
     });
-    $("#sidebar-dark-theme").on("click" , function(){
+    $("#sidebar-dark-theme").on("click", function() {
       $body.removeClass(sidebar_classes);
       $body.addClass("sidebar-dark");
+      $(".sidebar-bg-options").removeClass("selected");
+      $(this).addClass("selected");
+    });
+    $("#sidebar-light-theme").on("click", function() {
+      $body.removeClass(sidebar_classes);
+      $body.addClass("sidebar-light");
       $(".sidebar-bg-options").removeClass("selected");
       $(this).addClass("selected");
     });
 
 
     //Navbar Backgrounds
-    $(".tiles.primary").on("click" , function(){
+    $(".tiles.primary").on("click", function() {
       $(".navbar").removeClass(navbar_classes);
       $(".navbar").addClass("navbar-primary");
       $(".tiles").removeClass("selected");
       $(this).addClass("selected");
     });
-    $(".tiles.success").on("click" , function(){
+    $(".tiles.success").on("click", function() {
       $(".navbar").removeClass(navbar_classes);
       $(".navbar").addClass("navbar-success");
       $(".tiles").removeClass("selected");
       $(this).addClass("selected");
     });
-    $(".tiles.warning").on("click" , function(){
+    $(".tiles.warning").on("click", function() {
       $(".navbar").removeClass(navbar_classes);
       $(".navbar").addClass("navbar-warning");
       $(".tiles").removeClass("selected");
       $(this).addClass("selected");
     });
-    $(".tiles.danger").on("click" , function(){
+    $(".tiles.danger").on("click", function() {
       $(".navbar").removeClass(navbar_classes);
       $(".navbar").addClass("navbar-danger");
       $(".tiles").removeClass("selected");
       $(this).addClass("selected");
     });
-    $(".tiles.light").on("click" , function(){
-      $(".navbar").removeClass(navbar_classes);
-      $(".navbar").addClass("navbar-light");
-      $(".tiles").removeClass("selected");
-      $(this).addClass("selected");
-    });
-    $(".tiles.info").on("click" , function(){
+    $(".tiles.info").on("click", function() {
       $(".navbar").removeClass(navbar_classes);
       $(".navbar").addClass("navbar-info");
       $(".tiles").removeClass("selected");
       $(this).addClass("selected");
     });
-    $(".tiles.dark").on("click" , function(){
+    $(".tiles.dark").on("click", function() {
       $(".navbar").removeClass(navbar_classes);
       $(".navbar").addClass("navbar-dark");
       $(".tiles").removeClass("selected");
       $(this).addClass("selected");
     });
-    $(".tiles.default").on("click" , function(){
-      $(".navbar").removeClass(navbar_classes);
-      $(".tiles").removeClass("selected");
-      $(this).addClass("selected");
-    });
-    $(".tiles.default").on("click" , function(){
+    $(".tiles.default").on("click", function() {
       $(".navbar").removeClass(navbar_classes);
       $(".tiles").removeClass("selected");
       $(this).addClass("selected");
     });
 
-    $(".color-theme.default").click(function(){
-      $(".color-theme.default").attr({
-        "href" : "https://www.bootstrapdash.com/demo/star-admin2-pro/template/demo/vertical-default-light/index.html",
-        "title" : "Light"
-      });
+    //Horizontal menu in mobile
+    $('[data-toggle="horizontal-menu-toggle"]').on("click", function() {
+      $(".horizontal-menu .bottom-navbar").toggleClass("header-toggled");
     });
-    $(".color-theme.dark").click(function(){
-      $(".color-theme.dark").attr({
-        "href" : "https://www.bootstrapdash.com/demo/star-admin2-pro/template/demo/vertical-default-dark/index.html",
-        "title" : "Dark"
-      });
+    // Horizontal menu navigation in mobile menu on click
+    var navItemClicked = $('.horizontal-menu .page-navigation >.nav-item');
+    navItemClicked.on("click", function(event) {
+      if(window.matchMedia('(max-width: 991px)').matches) {
+        if(!($(this).hasClass('show-submenu'))) {
+          navItemClicked.removeClass('show-submenu');
+        }
+        $(this).toggleClass('show-submenu');
+      }        
     });
-    $(".color-theme.brown").click(function(){
-      $(".color-theme.brown").attr({
-        "href" : "https://www.bootstrapdash.com/demo/star-admin2-pro/template/demo/vertical-default-brown/index.html",
-        "title" : "Brown"
-      });
+
+    $(window).scroll(function() {
+      if(window.matchMedia('(min-width: 992px)').matches) {
+        var header = $('.horizontal-menu');
+        if ($(window).scrollTop() >= 71) {
+          $(header).addClass('fixed-on-scroll');
+        } else {
+          $(header).removeClass('fixed-on-scroll');
+        }
+      }
     });
+
+    $("#layout-toggler").on("click", function () {
+      $("#layout-settings").addClass("open");
+    });
+    $("#chat-toggler").on("click", function () {
+      $("#right-sidebar").addClass("open");
+    });
+
   });
 })(jQuery);
